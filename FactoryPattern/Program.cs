144fc65b58414ece45c35133e1a1ddb0a724e779
Felipemart123 <felipemart1876@gmail.com>
2022-07-20 12:10:42 -0400

    factory pattern

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -3,14 +3,13 @@ using FactoryPattern;
 
 Console.WriteLine("Enter the amount of tires you want.");
 int numOfWheels;
-bool input = false;
-do
-{
-    input = int.TryParse(Console.ReadLine(), out numOfWheels);
+
+
+    var input = int.TryParse(Console.ReadLine(), out numOfWheels);
 
     var vehicle = VehicleFactory.GetVehicle(numOfWheels);
 
-    Console.WriteLine(input);
+    //Console.WriteLine(input);
     //Console.WriteLine();
     vehicle.Drive();
-} while (!input);
+
